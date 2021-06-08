44271028f4ec976e798a2a48db04fd9dd628f4e2
alex <alexluc2001@gmail.com>
2021-06-07 17:11:25 -0700

    name change

--- sumcalc.py
+++ sumcalc.py
@@ -1,4 +1,4 @@
-print("Addition Calculator by Alex")
+print("Addition Calculator by Alex Lu")
 num1 = input("Enter the first number")
 num2 = input("Enter the second number")
 sum = float(num1) + float(num2)
